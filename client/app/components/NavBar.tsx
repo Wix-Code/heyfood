4dec2fa50577a2a119cc1e3ff3e4c2130890a382
Wix-Code <ogbonna428alex@gmail.com>
2025-05-04 01:13:04 +0300

    push

--- client/app/components/NavBar.tsx
+++ client/app/components/NavBar.tsx
@@ -7,9 +7,12 @@ import SearchIcon from '@mui/icons-material/Search';
 import TableRowsIcon from '@mui/icons-material/TableRows';
 import DesktopSideBar from './DesktopSideBar';
 import SearchAndSortNavbar from './SearchAndSortNavbar';
+import CloseIcon from '@mui/icons-material/Close';
+import SearchRestaurants from './SearchRestaurants';
 
 const NavBar = () => {
   const [openDesktopSidebar, setOpenDesktopSidebar] = useState<boolean>(false)
+  const [openSearch, setOpenSearch] = useState(false)
   return (
     <div style={{boxShadow: "rgba(0, 0, 0, 0.1) 0px 0px 5px 0px, rgba(0, 0, 0, 0.1) 0px 0px 1px 0px"}} className='sticky top-0 z-50 bg-[#FFFFFF]'>
       <div className='relative'>
@@ -27,20 +30,31 @@ const NavBar = () => {
           </div>
           <div className='bg-[#ececec] max-sm:hidden max-lg:w-[200px] px-3 py-[10px] items-center rounded-[40px] flex gap-2 w-[400px]'>
             <SearchIcon />
-            <input type="text" className='w-full outline-0 text-[14px]' placeholder='Search restaurants or food' />
+            <input onClick={() => setOpenSearch(true)} type="text" className='w-full outline-0 text-[14px]' placeholder='Search restaurants or food' />
           </div>
-          <div className='flex max-lg:mr-5 gap-3 items-center'>
-            <p className='font-[600] max-sm:hidden text-[14px]'>SIGN IN</p>
-            <div className='bg-[#000000] font-[600] text-[14px] px-6 py-2 rounded-[30px] flex gap-2 items-center text-[#FFFFFF]'>
-              <ShoppingCartIcon sx={{ fontSize: 18 }} />
-              <p className='max-sm:hidden'>CART</p>
-              <span>0</span>
-            </div>
-          </div>
+          {
+            openSearch ? (
+              <button className='cursor-pointer max-sm:mr-5' onClick={() => setOpenSearch(false)}><CloseIcon /></button>
+            ) : (
+                <>
+                  <div className='flex max-lg:mr-5 gap-3 items-center'>
+                    <p className='font-[600] max-sm:hidden text-[14px]'>SIGN IN</p>
+                    <div className='bg-[#000000] font-[600] text-[14px] px-6 py-2 rounded-[30px] flex gap-2 items-center text-[#FFFFFF]'>
+                      <ShoppingCartIcon sx={{ fontSize: 18 }} />
+                      <p className='max-sm:hidden'>CART</p>
+                      <span>0</span>
+                    </div>
+                  </div>
+                </>
+            )
+          }
         </div>
       </div>
       <div className='pb-3'>
         <SearchAndSortNavbar />
+        {
+          openSearch && ( <SearchRestaurants setOpenSearch={setOpenSearch} />)
+        }
       </div>
     </div>
   )
--- client/app/components/SearchRestaurants.tsx
+++ client/app/components/SearchRestaurants.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { component } from '../dummyData';
 
-const SearchRestaurants = () => {
+const SearchRestaurants = ({setOpenSearch}:{setOpenSearch : React.Dispatch<React.SetStateAction<boolean>>;}) => {
   return (
-    <div>SearchRestaurants</div>
+    <div className='absolute bg-[#FFFFFF] overflow-y-scroll w-full h-screen'>
+      <div>
+        <h1 className='ml-20 max-sm:ml-5 text-[22px] tracking-[1px] my-5 font-[600]'>Categories</h1>
+        <div>
+          {
+            component.map((item) => {
+              return (
+                <div key={item.id} className='py-7 max-sm:p-5 cursor-pointer hover:bg-[#f7f7f7] px-20 border-[#f7f7f7] border-b-[1px]'>
+                  <p className='text-[16px] tracking-[1px] font-[600]'>{item.name}</p>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+    </div>
   )
 }
 
