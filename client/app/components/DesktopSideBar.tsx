826e04571f992d24b6f823be31c096e652a76afc
Wix-Code <ogbonna428alex@gmail.com>
2025-05-05 16:57:51 +0300

    push

--- client/app/components/DesktopSideBar.tsx
+++ client/app/components/DesktopSideBar.tsx
@@ -9,7 +9,7 @@ const DesktopSideBar = ({
   }) => {
   return (
     <div className='absolute z-40 bg-[#00000062] top-0 left-0 w-full h-screen'>
-      <div className='bg-[#FFFFFF] flex flex-col py-10 justify-between h-screen max-sm:w-[90%] w-[20%]'>
+      <div className='bg-[#FFFFFF] flex flex-col max-sm:pb-20 py-10 justify-between h-screen max-sm:w-[90%] w-[20%]'>
         <div className='flex justify-end mr-4'>
           <button onClick={() => setOpenDesktopSidebar(false)} className='text-right cursor-pointer w-[45px] h-[45px] flex items-center justify-center rounded-[50%] hover:bg-[#f1f1f1]'><CloseIcon /></button>
         </div>
@@ -22,7 +22,7 @@ const DesktopSideBar = ({
           <p className='text-[16px] cursor-pointer px-6 py-4 hover:bg-[#f1f1f1] font-[600] text-[#111111]'>Become a delivery rider</p>
           <p className='text-[16px] cursor-pointer px-6 py-4 hover:bg-[#f1f1f1] font-[600] text-[#111111]'>Go to Homepage</p>
         </div>
-        <div className='ml-6 max-sm:mb-5'>
+        <div className='ml-6 max-sm:mb-10'>
           <div className='flex mb-4 items-center gap-3'>
             <img src="https://heyfood.africa/icons/new/logo-circle-green.svg" alt="heyfood icon" />
             <p className='text-[16px] font-[600] text-[#111111]'>Experience the <br />
